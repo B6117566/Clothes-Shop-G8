137d0a1d06a072719d3eb8bf603d918051a5fc47
B6117566 <b6117566@g.sut.ac.th>
2021-06-12 03:18:04 +0700

    add manageProduct and cardItemManage

---
+++ frontend/src/app/components/manage-product/manage-card-item-list/manage-card-item-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-card-item-list',
+  templateUrl: './manage-card-item-list.component.html',
+  styleUrls: ['./manage-card-item-list.component.css']
+})
+export class ManageCardItemListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
