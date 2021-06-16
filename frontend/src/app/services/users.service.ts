83686cd29daaf893993ece5bebc966cc23eddc08
B6117566 <b6117566@g.sut.ac.th>
2021-06-16 23:37:51 +0700

    update user service

--- frontend/src/app/services/users.service.ts
+++ frontend/src/app/services/users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { LocalStorageService } from 'angular-web-storage';
 import { map } from 'rxjs/operators';
 
@@ -7,7 +7,6 @@ import { map } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class UsersService {
-
   usertype_id: any;
 
   constructor(private http: HttpClient, public local: LocalStorageService) {}
@@ -26,6 +25,40 @@ export class UsersService {
       );
   }
 
+  getUserData() {
+    let token = this.local.get('user').token;
+    let user_id = this.local.get('user').result.id;
+    return this.http.get<any>(
+      'http://localhost:3000/api/users/get/id/' + user_id,
+      {
+        headers: new HttpHeaders().set('Authorization', token),
+      }
+    );
+  }
+
+  updateUserData(data: any) {
+    let token = this.local.get('user').token;
+    let user_id = this.local.get('user').result.id;
+    let body_send = JSON.stringify([
+      { id: data.id },
+      {
+        email: data.email,
+        password: data.password,
+        firstname: data.firstname,
+        lastname: data.lastname,
+        phone: data.phone,
+        address: data.address,
+      },
+    ]);
+    return this.http.put<any>(
+      'http://localhost:3000/api/users/get/id/' + user_id,
+      body_send,
+      {
+        headers: new HttpHeaders().set('Authorization', token),
+      }
+    );
+  }
+
   signUp(signupData: any) {
     return this.http.post<any>(
       'http://localhost:3000/api/users/signup',
@@ -34,14 +67,16 @@ export class UsersService {
   }
 
   getUsertype(data: string) {
-    return this.http.get<any>('http://localhost:3000/api/usertypes/get/' + data).pipe(
-      map((data) => {
-        if (data) {
-          this.usertype_id = data;
-          console.log(this.usertype_id);
-        }
-        return this.usertype_id;
-      })
-    );
+    return this.http
+      .get<any>('http://localhost:3000/api/usertypes/get/' + data)
+      .pipe(
+        map((data) => {
+          if (data) {
+            this.usertype_id = data;
+            console.log(this.usertype_id);
+          }
+          return this.usertype_id;
+        })
+      );
   }
 }
