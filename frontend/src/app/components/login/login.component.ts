87f09670857d0690928efa59baba0c3ea640e44a
B6117566 <b6117566@g.sut.ac.th>
2021-06-16 23:02:09 +0700

    update login

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,25 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required])
-  })
+    password: new FormControl('', [Validators.required]),
+  });
 
-  constructor() { }
+  constructor(private signin: UsersService,private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onClick() {
+    console.log(this.loginForm.value);
+    this.signin.signIn(this.loginForm.value).subscribe(
+      (data) => {
+        if (data.status == true) {
+          this.router.navigate(['']);
+        } else {
+          alert('Email or Password is incorrect');
+        }
+      },
+      (err) => {
+        console.log(err);
+        alert('Email or Password is incorrect');
+      }
+    );
   }
 
-  onClick(){
-    console.log(this.loginForm.value)
+  get loginFromData(){
+    return this.loginForm.controls
   }
-
 }
