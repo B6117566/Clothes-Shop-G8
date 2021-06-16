c2669e78c29e0f9abc3832e57512501d357da7cc
B6117566 <b6117566@g.sut.ac.th>
2021-06-17 00:28:39 +0700

    update cart and order

--- backend/api/cart.js
+++ backend/api/cart.js
@@ -2,102 +2,61 @@ var expressFunction = require("express");
 const router = expressFunction.Router();
 
 const authorization = require("../config/authorize");
-const Product = require("../model/product.model");
+const Cart = require("../model/cart.model");
 
 //--------------------------------------------------------------------------
 
-const getProducts = async () => {
+const findCartByID = async (id) => {
   return new Promise((resolve, reject) => {
-    Product.find({}, (err, data) => {
+    Cart.find({ _id: id }, (err, data) => {
       if (err) {
-        reject(new Error("Cannot get Products"));
+        reject(new Error("Cannot find Carts"));
       } else {
         if (data.length != 0) {
           resolve(data);
         } else {
-          reject(new Error("Cannot get Products"));
+          reject(new Error("Cannot find Carts"));
         }
       }
-    })
-      .populate("gender_id")
-      .populate("typeproduct_id")
-      .populate("quantity.size_id");
+    }).populate("product_id");
   });
 };
 
-const findProducts = async (data) => {
+const insertCart = async (data) => {
   return new Promise((resolve, reject) => {
-    Product.find({ name: { $regex: data } }, (err, data) => {
+    const new_Cart = new Cart(data);
+    new_Cart.save((err, data) => {
       if (err) {
-        reject(new Error("Cannot find Products"));
+        reject(new Error("Cannot insert Cart to DB"));
       } else {
-        if (data.length != 0) {
-          resolve(data);
-        } else {
-          reject(new Error("Cannot find Products"));
-        }
-      }
-    })
-      .populate("gender_id")
-      .populate("typeproduct_id")
-      .populate("quantity.size_id");
-  });
-};
-
-const findProductByID = async (id) => {
-  return new Promise((resolve, reject) => {
-    Product.findOne({ _id: id }, (err, data) => {
-      if (err) {
-        reject(new Error("Cannot find Product By ID"));
-      } else {
-        if (data) {
-          resolve(data);
-        } else {
-          reject(new Error("Cannot find Product By ID"));
-        }
-      }
-    })
-      .populate("gender_id")
-      .populate("typeproduct_id")
-      .populate("quantity.size_id");
-  });
-};
-
-const insertProduct = async (data) => {
-  return new Promise((resolve, reject) => {
-    const new_product = new Product(data);
-    new_product.save((err, data) => {
-      if (err) {
-        reject(new Error("Cannot insert Product to DB"));
-      } else {
-        resolve({ message: "Product added successfully." });
+        resolve({ message: "Cart added successfully." });
       }
     });
   });
 };
 
-const deleteProduct = async (id) => {
+const deleteCart = async (id) => {
   return new Promise((resolve, reject) => {
-    Product.deleteOne({ _id: id }, (err, data) => {
+    Cart.deleteOne({ _id: id }, (err, data) => {
       if (err) {
-        reject(new Error("Cannot Delete Product"));
+        reject(new Error("Cannot Delete Cart"));
       } else {
-        resolve({ message: "Product delete successfully." });
+        resolve({ message: "Cart delete successfully." });
       }
     });
   });
 };
 
-const updateProduct = async (id, data) => {
+const updateCart = async (id, data) => {
   return new Promise((resolve, reject) => {
     if (id == undefined) {
-      reject(new Error("Cannot update Product"));
+      reject(new Error("Cannot update Cart"));
     }
-    Product.updateOne({ _id: id }, { $set: data }, (err, data) => {
+    Cart.updateOne({ _id: id }, { $set: data }, (err, data) => {
       if (err) {
-        reject(new Error("Cannot update Product"));
+        reject(new Error("Cannot update Cart"));
       } else {
-        resolve({ message: "Product update successfully." });
+        resolve({ message: "Cart update successfully." });
       }
     });
   });
@@ -105,8 +64,8 @@ const updateProduct = async (id, data) => {
 
 //--------------------------------------------------------------------------
 
-router.route("/get").get((req, res) => {
-  getProducts()
+router.route("/get/id/:ID").get(authorization, (req, res) => {
+  findCartByID(req.params.ID)
     .then((result) => {
       res.status(200).json(result);
     })
@@ -115,29 +74,8 @@ router.route("/get").get((req, res) => {
     });
 });
 
-router.route("/get/:search").get((req, res) => {
-  findProducts(new RegExp(req.params.search))
-    .then((result) => {
-      res.status(200).json(result);
-    })
-    .catch((err) => {
-      res.status(404).send(String(err));
-    });
-});
-
-router.route("/get/id/:ID").get((req, res) => {
-  findProductByID(req.params.ID)
-    .then((result) => {
-      res.status(200).json(result);
-    })
-    .catch((err) => {
-      res.status(404).send(String(err));
-    });
-});
-
-//router.route("/add").post(authorization, (req, res) => {
-router.route("/add").post((req, res) => {
-  insertProduct(req.body)
+router.route("/add").post(authorization, (req, res) => {
+  insertCart(req.body)
     .then((result) => {
       res.status(201).json(result);
     })
@@ -146,9 +84,8 @@ router.route("/add").post((req, res) => {
     });
 });
 
-//router.route("/del").delete(authorization, (req, res) => {
-router.route("/del/:id").delete((req, res) => {
-  deleteProduct(req.params.id)
+router.route("/del/:id").delete(authorization, (req, res) => {
+  deleteCart(req.params.id)
     .then((result) => {
       res.status(200).json(result);
     })
@@ -157,10 +94,9 @@ router.route("/del/:id").delete((req, res) => {
     });
 });
 
-//router.route("/edit").put(authorization, (req, res) => {
-router.route("/put").put((req, res) => {
+router.route("/put").put(authorization, (req, res) => {
   //----req[0] ID, req[1] ช้อมูล
-  updateProduct(req.body[0].id, req.body[1])
+  updateCart(req.body[0].id, req.body[1])
     .then((result) => {
       res.status(200).json(result);
     })
